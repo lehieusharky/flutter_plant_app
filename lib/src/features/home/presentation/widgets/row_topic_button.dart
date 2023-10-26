061fed126424285c46a0bcec396e07b3663bac40
lehuy22012004 <daithanhmb3@gmail.com>
2023-10-26 14:51:43 +0700

    [Bug] Scroll when click topic button

--- lib/src/features/home/presentation/widgets/row_topic_button.dart
+++ lib/src/features/home/presentation/widgets/row_topic_button.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:plant_market/src/core/data/defines/constants/image_constant.dart';
+import 'package:plant_market/src/core/di/part_di.dart';
 import 'package:plant_market/src/core/extension/localization.dart';
+import 'package:plant_market/src/core/extension/responsive.dart';
+import 'package:plant_market/src/core/presentation/custom_widgets/custom_modal.dart';
 import 'package:plant_market/src/features/home/data/enum/topic_symbol.dart';
 import 'package:plant_market/src/features/home/presentation/bloc/home_page_bloc.dart';
 import 'package:plant_market/src/features/home/presentation/widgets/part_home_page_widget.dart';
 
 class RowTopicButton extends StatefulWidget {
-  const RowTopicButton({super.key});
+  final ScrollController pageScrollController;
+  const RowTopicButton({super.key, required this.pageScrollController});
 
   @override
   State<RowTopicButton> createState() => _RowTopicButtonState();
@@ -22,6 +26,9 @@ class _RowTopicButtonState extends State<RowTopicButton> {
       listener: (context, state) {
         if (state is HomePageChangeTopicSuccess) {
           _isChoosedTopic = state.topicSymbol;
+          widget.pageScrollController.animateTo(240,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut);
         }
       },
       builder: (context, state) {
@@ -29,20 +36,29 @@ class _RowTopicButtonState extends State<RowTopicButton> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             TopicButton(
-                onPressed: () => _changeTopic(topicSymbol: TopicSymbol.general),
+                onPressed: () {
+                  _showPlantIdentify(context);
+                  _changeTopic(topicSymbol: TopicSymbol.general);
+                },
                 title: translate(context).general,
                 pathIcon: imageConstant.leavesSVG,
                 isChoosed: _isChoosedTopic,
                 topicSymbol: TopicSymbol.general),
             TopicButton(
-              onPressed: () => _changeTopic(topicSymbol: TopicSymbol.community),
+              onPressed: () {
+                _showCreateCommunityPost(context);
+                _changeTopic(topicSymbol: TopicSymbol.community);
+              },
               title: translate(context).community,
               pathIcon: imageConstant.communitySVG,
               isChoosed: _isChoosedTopic,
               topicSymbol: TopicSymbol.community,
             ),
             TopicButton(
-              onPressed: () => _changeTopic(topicSymbol: TopicSymbol.map),
+              onPressed: () {
+                _showImageGalerry(context);
+                _changeTopic(topicSymbol: TopicSymbol.map);
+              },
               title: translate(context).map,
               pathIcon: imageConstant.mapSVG,
               isChoosed: _isChoosedTopic,
@@ -51,6 +67,39 @@ class _RowTopicButtonState extends State<RowTopicButton> {
           ],
         );
       },
+    );
+  }
+
+  void _showImageGalerry(BuildContext context) {
+    CustomModal.baseModal(
+      context: context,
+      height: context.height * 0.8,
+      child: Text(
+        'Image galerry',
+        style: theme(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  void _showCreateCommunityPost(BuildContext context) {
+    CustomModal.baseModal(
+      context: context,
+      height: context.height * 0.8,
+      child: Text(
+        'Community post',
+        style: theme(context).textTheme.titleMedium,
+      ),
+    );
+  }
+
+  void _showPlantIdentify(BuildContext context) {
+    CustomModal.baseModal(
+      context: context,
+      height: context.height * 0.8,
+      child: Text(
+        'Community post',
+        style: theme(context).textTheme.titleMedium,
+      ),
     );
   }
 
