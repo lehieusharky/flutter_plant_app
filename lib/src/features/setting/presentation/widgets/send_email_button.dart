a523c1b02cd7e760d5ae1d2c71a99c6931a79e58
lehieusharky <lehieultt@gmail.com>
2023-10-31 15:02:16 +0700

    [SettingPage] Send email logic

---
+++ lib/src/features/setting/presentation/widgets/send_email_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:plant_market/src/core/data/defines/constants/image_constant.dart';
+import 'package:plant_market/src/core/extension/localization.dart';
+import 'package:plant_market/src/features/setting/presentation/widgets/setting_button.dart';
+
+class SendEmailButton extends StatefulWidget {
+  const SendEmailButton({super.key});
+
+  @override
+  State<SendEmailButton> createState() => _SendEmailButtonState();
+}
+
+class _SendEmailButtonState extends State<SendEmailButton> {
+  
+  final Email email = Email(
+    body: 'noi dung can bo sung',
+    subject: 'Gop y bo sung cho app',
+    recipients: [],
+    cc: ['lehieultt@gmail.com'],
+    attachmentPaths: [],
+    isHTML: false,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SettingButton(
+      title: translate(context).contactWithUs,
+      field: '',
+      onPressed: () async {
+        await FlutterEmailSender.send(email);
+      },
+      iconTitlePath: imageConstant.contactUsSVG,
+    );
+  }
+}
